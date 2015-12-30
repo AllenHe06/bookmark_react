87fb89f1e25959a6ae49ee5f8447dc156b48f4e9
yuhua he <yuhuahe@buffalo.edu>
2015-12-30 01:46:18 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+var links = [
+  { title: "fake link 1", url: "fake.url.1"},
+  { title: "fake link 2", url: "fake.url.2"},
+  { title: "fake link 3", url: "fake.url.3"}
+];
+
+router.get('/api/links', function(req, res, next) {
+  res.json({ links: links });
+});
+
+router.post('/api/links', function(req, res, next) {
+  var newLink = req.body;
+  links.push(newLink);
+  res.json(newLink);
+});
+
+module.exports = router;
